f6416f08b2aad7288017550de7ab473e16838b67
Aleksandra Półtorak <xmacabrax@gmail.com>
2018-09-18 20:46:22 +0200

    Added about me and contact section

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-
 $(function () {
     $("#s1-text").load("about-me/about-me.html", function (response, status, xhr) {
         if (status == "error") {
@@ -43,43 +42,54 @@ $(function () {
 
 const app = (() => {
     $(".navbar").addClass("nav-inactive");
-    $(".nav-icon").on("click",(()=>{
+    $(".nav-icon__line").addClass("nav-icon-inactive");
+    $(".nav-icon").on("click", (() => {
         //alert("KLIK");
-        $(".navbar").toggleClass("nav-inactive","nav-active");
+        $(".navbar").toggleClass("nav-inactive", "nav-active");
+        $(".nav-icon__line").toggleClass("nav-icon-inactive", "nav-icon-active");
+        $(".nav-icon__line").addClass("nav-icon-active-2");
+        $(".nav-icon__line__left").addClass("nav-icon-active");
+        $(".nav-icon__line__right").addClass("nav-icon-active");
     }));
 
     var images = [].slice.call(document.querySelectorAll('.js-parallax-bg'));
+
     function getViewportHeight() {
         var a = document.documentElement.clientHeight, b = window.innerHeight;
         return a < b ? b : a;
     }
+
     function getViewportScroll() {
-        if(typeof window.scrollY != 'undefined') {
+        if (typeof window.scrollY != 'undefined') {
             return window.scrollY;
         }
-        if(typeof pageYOffset != 'undefined') {
+        if (typeof pageYOffset != 'undefined') {
             return pageYOffset;
         }
         var doc = document.documentElement;
         doc = doc.clientHeight ? doc : document.body;
         return doc.scrollTop;
     }
+
     function doParallax() {
         console.log("Scrolling");
         var el, elOffset, elHeight,
             offset = getViewportScroll(),
             vHeight = getViewportHeight();
 
-        for(var i in images) {
+        for (var i in images) {
             el = images[i];
             elOffset = el.offsetTop;
             elHeight = el.offsetHeight;
 
-            if((elOffset > offset + vHeight) || (elOffset + elHeight < offset)) { continue; }
+            if ((elOffset > offset + vHeight) || (elOffset + elHeight < offset)) {
+                continue;
+            }
 
-            el.style.backgroundPosition = '50% '+Math.round((elOffset - offset)*3/8)+'px';
+            el.style.backgroundPosition = '50% ' + Math.round((elOffset - offset) * 3 / 8) + 'px';
         }
     }
+
     1
     Gator(window).on('scroll', doParallax);
 
