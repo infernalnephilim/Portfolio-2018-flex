7fc553bf20713c0e07f05fa12c7c882a0256a580
Aleksandra Półtorak <xmacabrax@gmail.com>
2018-10-10 16:53:54 +0200

    Dodanie elementu do parallax

--- js/script.js
+++ js/script.js
@@ -74,15 +74,15 @@ $(document).ready(function () {
     });
 
 
-    var images = [].slice.call(document.querySelectorAll('.js-parallax-bg'));
+    const images = [].slice.call(document.querySelectorAll('.js-parallax-bg'));
+    const imagesCover = [].slice.call(document.querySelectorAll('.js-parallax-bg-cover'));
 
     function doParallax() {
-        console.log("Scrolling");
-        var el, elOffset, elHeight,
+        let el, elOffset, elHeight,
             offset = getViewportScroll(),
             vHeight = getViewportHeight();
 
-        for (var i in images) {
+        for (let i in images) {
             el = images[i];
             elOffset = el.offsetTop;
             elHeight = el.offsetHeight;
@@ -92,6 +92,18 @@ $(document).ready(function () {
             }
 
             el.style.backgroundPosition = '50% ' + Math.round((elOffset - offset) * 3 / 8) + 'px';
+        }
+        for (let i in imagesCover) {
+            el = imagesCover[i];
+            elOffset = el.offsetTop;
+            elHeight = el.offsetHeight;
+
+            if ((elOffset > offset + vHeight) || (elOffset + elHeight < offset)) {
+                continue;
+            }
+
+            el.style.backgroundPosition = '50% ' + Math.round((elOffset - offset) * 9 / 8 - 50) + 'px';
+            console.log(el.style.backgroundPosition);
         }
     }
 
@@ -120,7 +132,7 @@ function getViewportScroll() {
     if (typeof pageYOffset != 'undefined') {
         return pageYOffset;
     }
-    const doc = document.documentElement;
+    let doc = document.documentElement;
     doc = doc.clientHeight ? doc : document.body;
     return doc.scrollTop;
 }
